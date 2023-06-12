7cd8e8fffa953df539d0d76892b8a88e87b8df3a
simonepietraroia <simonepietraroia21@gmail.com>
2023-06-12 15:17:31 +0000

    added the display menu function to serve as a user interface

--- run.py
+++ run.py
@@ -1,16 +1,29 @@
 import json
 
 def display_menu():
+    """
+    This function displays the different task options that 
+    this application provides.
+    """
+    print("-----Task-Planner-----")
+    print("1. Add Task")
+    print("2. View Task")
+    print("3. Mark Task As Complete")
+    print("4. Save Tasks")
+    print("5. Load Tasks")
+    print("6. Exit")
 
-def add_task():
+# def add_task():
 
-def view_task():
+# def view_task():
 
-def complete_task():
+# def complete_task():
 
-def save_task():
+# def save_task():
 
-def load_task():
+# def load_task():
 
 def main():
+    display_menu()
 
+main()
